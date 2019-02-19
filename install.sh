bc9926446965ff5a2ef88c325562e46465d9e225
Timothy Lock <lock.k.timothy@gmail.com>
2019-02-19 00:31:39 -0500

    Fix flask install for sudo

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ sudo apt-get -y install python3-pip
 echo Installing Flask
 pip3 install flask
 
+echo Installing Flask for Sudo to use during bootup
+sudo pip3 install flask
+
 echo Setting server to autostart
 sudo sed -i "`wc -l < /etc/rc.local`i\\python3 $PWD/main.py &\\" /etc/rc.local
 
