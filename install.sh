09540efb178290515a069e63576eb67081f66539
Timothy Lock <me@timothylock.ca>
2022-05-03 19:48:12 -0400

    Move Flask to requirements.txt

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 echo Installing pip
 sudo apt-get -y install python3-pip
 
-echo Installing Flask
-pip3 install flask
-
-echo Installing Flask for Sudo to use during bootup
-sudo pip3 install flask
+echo Installing Requirements
+sudo pip3 install -r requirements.txt
 
 echo Setting server to autostart
 sudo sed -i "`wc -l < /etc/rc.local`i\\python3 $PWD/main.py &\\" /etc/rc.local
-
